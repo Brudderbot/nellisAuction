22647e5cfd8e189037c1875eac748133637115c3
brudderbot <nathan.a@team8bit.com>
2023-10-25 14:00:22 -0700

    first commit

---
+++ example.js
@@ -0,0 +1,119 @@
+const product = {
+    "bidState": {
+        "isActiveUserWinning": false,
+        "projectId": 18985422,
+        "publicMaxBid": null,
+        "userMaxBid": null,
+        "userNextMinBid": 30
+    },
+    "dateClosed": {
+        "__type": "Date",
+        "value": "2023-10-26T02:17:00.000Z"
+    },
+    "marketStatus": "open",
+    "title": "Ninja NC301 CREAMi Ice Cream Maker, for Gelato, Mix-ins, Milkshakes, Sorbet, Smoothie Bowls & More, 7 One-Touch Programs, with (2) Pint Containers & Lids, Compact Size, Perfect for Kids, Silver\n",
+    "wprice": 29,
+    "location": {
+        "address": "1402 S 40th Ave",
+        "zipCode": 85009,
+        "offsite": false,
+        "pickupDetail": "",
+        "timezone": "America/Phoenix",
+        "city": "Phoenix",
+        "name": "Phoenix",
+        "state": "AZ"
+    },
+    "_count": {
+        "bids": 11
+    },
+    "originType": "revalidate",
+    "bprice": 0,
+    "createdAt": {
+        "__type": "Date",
+        "value": "2023-10-24T17:21:51.523Z"
+    },
+    "dateAdded": {
+        "__type": "Date",
+        "value": "2023-10-25T03:46:00.651Z"
+    },
+    "id": 18985422,
+    "rprice": 1,
+    "sold": 0,
+    "sprice": 1,
+    "updatedAt": {
+        "__type": "Date",
+        "value": "2023-10-25T14:27:42.418Z"
+    },
+    "item": {
+        "assemblyType": {
+            "id": 2,
+            "description": "No"
+        },
+        "conditionType": {
+            "id": 6,
+            "description": "Used"
+        },
+        "missingPartsType": {
+            "id": 6,
+            "description": "No"
+        },
+        "functionalType": {
+            "id": 1,
+            "description": "Yes"
+        },
+        "damageType": {
+            "id": 7,
+            "description": "None"
+        },
+        "packageType": {
+            "id": 5,
+            "description": "Yes"
+        },
+        "categoryType": {
+            "id": 6,
+            "description": "General Merchandise"
+        },
+        "photos": [
+            {
+                "url": "https://firebasestorage.googleapis.com/v0/b/nellishr-cbba0.appspot.com/o/processing-photos%2F1007549244%2FNreMBOay3Un5ie40zRkCW.jpg?alt=media&token=481f7ffd-46fc-4dbf-805d-08458d699534",
+                "name": "NreMBOay3Un5ie40zRkCW.jpg",
+                "fullPath": "processing-photos/1007549244/NreMBOay3Un5ie40zRkCW.jpg"
+            },
+            {
+                "url": "https://firebasestorage.googleapis.com/v0/b/nellishr-cbba0.appspot.com/o/processing-photos%2F1007549244%2FHLrU4UfMe5FJJssXoomA0.jpg?alt=media&token=dffd8c68-a5a0-4f43-b083-0b4ba45fa5e5",
+                "name": "HLrU4UfMe5FJJssXoomA0.jpg",
+                "fullPath": "processing-photos/1007549244/HLrU4UfMe5FJJssXoomA0.jpg"
+            },
+            {
+                "url": "https://firebasestorage.googleapis.com/v0/b/nellishr-cbba0.appspot.com/o/processing-photos%2F1007549244%2FVena0ZLEL1QoRg8jptFnh.jpg?alt=media&token=445d0116-11c2-4059-b164-5cee6d289912",
+                "name": "Vena0ZLEL1QoRg8jptFnh.jpg",
+                "fullPath": "processing-photos/1007549244/Vena0ZLEL1QoRg8jptFnh.jpg"
+            },
+            {
+                "url": "https://firebasestorage.googleapis.com/v0/b/nellishr-cbba0.appspot.com/o/processing-photos%2F1007549244%2F_oxiqZuP-5jJNwwj8VSpf.jpg?alt=media&token=1b4d337d-0b17-4425-afc1-ab4bd256389a",
+                "name": "_oxiqZuP-5jJNwwj8VSpf.jpg",
+                "fullPath": "processing-photos/1007549244/_oxiqZuP-5jJNwwj8VSpf.jpg"
+            }
+        ],
+        "inventoryNumber": "1007549244",
+        "retailPrice": 230,
+        "notes": null,
+        "starRating": 4.5,
+        "updatedAt": {
+            "__type": "Date",
+            "value": "2023-10-24T19:30:14.000Z"
+        },
+        "query": "cremi"
+    },
+    "buyNow": {}
+}
+const addMessage = {
+    "type": "add",
+    "query": "example"
+}
+const removeMessage = {
+    "type": "remove",
+    "query": "example"
+}
+
+
---
+++ index.ts
@@ -0,0 +1,152 @@
+
+
+const dataFile = Bun.file("data.json");
+const data = JSON.parse(await dataFile.text());
+
+
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str:string) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+/**
+ * checks for the hash of an item in a json array
+ * @param  {Array} list The json to check for the item in.
+ * @param  {any} item The item to check for.
+ * @return {boolean}
+ */
+function checkForItem(list:Array<Number>, item:any){
+    const hash = hashCode(item.dateAdded.value + item.title)
+
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] == hash){
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+/**
+ * Adds a query to the items.json file
+ * @param  {string} query The query to add the the items.json file.
+ * @return {boolean}
+ */
+async function addQuery(query:string){
+    const itemsFile = Bun.file("items.json");
+    const items = JSON.parse( await itemsFile.text());
+
+    items.push(query);
+    Bun.write(itemsFile, JSON.stringify(items));
+}
+
+/**
+ * Removes a query to the items.json file
+ * @param  {string} query The query to add the the items.json file.
+ * @return {boolean}
+ */
+async function removeQuery(query:string){
+    const itemsFile = Bun.file("items.json");
+    const items = JSON.parse( await itemsFile.text());
+
+    const index = items.indexOf(query);
+    if (index > -1) { // only splice array when item is found
+        items.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    Bun.write(itemsFile, JSON.stringify(items));
+}
+
+Bun.serve({
+    port: 8080,
+    fetch(req, server) {
+        // upgrade the request to a WebSocket
+        if (server.upgrade(req)) {
+          return; // do not return a Response
+        }
+        return new Response(Bun.file("./index.html"));
+      }, // upgrade logic
+    websocket: {
+        // code 500 is invalid json
+        // code 404 is type not found
+      async message(ws, message) {
+        try {
+            const msgJson = JSON.parse(message.toString())
+            switch (msgJson.type) {
+                case "add":
+                    addQuery(msgJson.query);
+                break;
+                    
+                case "remove":
+                    removeQuery(msgJson.query);
+                break;
+
+                case "get":
+                    const items = await Bun.file("items.json").text();
+                    ws.send(items);
+                break;
+    
+                default:
+                    ws.send("404")
+                    break;
+            }
+        } catch (error) {
+         ws.send("500")   
+        }
+      }, // a message is received
+      open(ws) {}, // a socket is opened
+      close(ws, code, message) {}, // a socket is closed
+      drain(ws) {}, // the socket is ready to receive more data
+    },
+  });
+
+async function main() {
+    const itemsText = await Bun.file("items.json").text();
+    const items = JSON.parse(itemsText);
+    for (let index = 0; index < items.length; index++) {
+        const query = items[index];
+        console.log("Checked " + query)
+        const response = await fetch("https://www.nellisauction.com/search?query=" + query + "&_data=routes%2Fsearch", {
+            headers: {
+                Cookie: '__shopping-location=eyJzaG9wcGluZ0xvY2F0aW9uIjp7ImlkIjoyLCJuYW1lIjoiUGhvZW5peCwgQVoifX0=.ViHixf0+jtriCgQyfRH5wAeOxRuXUjpFvWd8be/YlN4; Expires=Tue, 19 Jan 2038 03:14:07 GMT; Domain=www.nellisauction.com; Path=/search	'
+            }
+        });
+
+        const body = await response.json();
+        for (let index = 0; index < body.products.length; index++) {
+            const product = body.products[index];
+                var diff = Date.parse(product.dateClosed.value) - Date.now();
+
+                if (diff > 0 && diff > 39600 && product.sold == 0 && !checkForItem(data,product)) {
+                    product.item.query = query;
+                    data.push(hashCode(product.dateAdded.value + product.title));
+                    Bun.write(dataFile, JSON.stringify(data))
+                    console.log(product);
+                    fetch('https://ntfy.sh', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            "topic": "nellis",
+                            "message": "Current price: " + product.wprice + ", Retail price: " + product.item.retailPrice,
+                            "title": product.title,
+                            "attach": product.item.photos[0].url,
+                            "click": encodeURI("https://www.nellisauction.com/p/" + product.title.replace(/\s/g, "-") + "/" + product.bidState.projectId)
+                        })
+                    })
+
+                }
+        }
+    }
+}
+main();
+setInterval(main, 30000)
+
